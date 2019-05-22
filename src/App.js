9b27988897c1775f45974d748d187ca0fb14b36e
Simon Kong <simon.kong95@gmail.com>
2019-05-22 15:55:32 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Switch, Route } from 'react-router-dom'
 import Landing from './containers/Landing'
 import User from './containers/Profile'
 import NotFound from './components/404'
+import Navigation from './containers/Navigation'
 import './css/style.css';
 
 function App() {
   return (
-    <div className="app bg bg-full">
+    <div className="app bg">
+      <Navigation/>
       <Switch>
         <Route path='/user/steamid=:id' component={User}/>
         <Route path='/404' component={NotFound}/>
---
+++ src/containers/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Navigation extends Component {
+    render() {
+        return (
+            <nav className="nav">
+                hello
+            </nav>
+        )
+    }
+}
+
+export default Navigation
